49075f64255407776b0feb67a6a1efd65e3c154c
Muhammad Fikri bin Iskandar <muhammadfikri@student.usm.my>
2026-01-09 18:02:34 +0800

    Added Customer OOP class

---
+++ src/main/java/org/example/pixelhavencat201/Customer.java
@@ -0,0 +1,17 @@
+package org.example.pixelhavencat201;
+
+public class Customer {
+    private String fullName;
+    private String email;
+    private String password;
+
+    public Customer(String fullName, String email, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters - These allow the JSP to display the name
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
+}
